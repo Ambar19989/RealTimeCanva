0861e8fae56c54ac87a24a6099b7031df4d96ff8
Ambar Azad <ambar19989@gmail.com>
2025-11-09 12:22:37 +0530

    Update WebSocket server URL to production backend

--- Client/websocket.js
+++ Client/websocket.js
@@ -1,5 +1,5 @@
 export class WebSocketClient {
-  constructor(serverUrl = 'http://localhost:3000') {
+  constructor(serverUrl = 'https://real-time-canva-server.onrender.com') {
     this.serverUrl = serverUrl;
     this.socket = null;
     this.connected = false;
